dec59e812fb24fe6debb1c12261b68031566284e
Hyoungju Lim <hyoungju03@gmail.com>
2026-04-20 00:12:19 +0900

    added automatic localhost checking

--- receiver.c
+++ receiver.c
@@ -1,18 +1,28 @@
 #include <stdio.h>
-#include <ifaddrs.h>
-#include <sys/types.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <inttypes.h>
-#include <errno.h>
-#include <string.h>
 #include <net/if.h>
 
 
-int main()
+char *ipv6_addr = "fe80::101b:6c28:18f4:48a6";
+char *if_name = "en0";
+
+
+int main(int argc, char *argv[])
 {
+    // check localhost setting
+    int opt;
+    while ((opt = getopt(argc, argv, "l")) != -1)
+    {
+        if (opt == 'l')
+        {
+            ipv6_addr = "::1";
+            if_name = "lo";
+        }
+    }
     
     int sfd = socket(AF_INET6, SOCK_STREAM, 0);
     if (sfd == -1)
@@ -24,11 +34,11 @@ int main()
     struct sockaddr_in6 sock_addr;
     sock_addr.sin6_family = AF_INET6;
     sock_addr.sin6_port = htons(8080);
-    sock_addr.sin6_scope_id = if_nametoindex("en0");
+    sock_addr.sin6_scope_id = if_nametoindex(if_name);
 
     // printf("sock_addr size: %zu (B)\n", sizeof(sock_addr));
 
-    const char *ipv6_addr = "fe80::101b:6c28:18f4:48a6";
+    //const char *ipv6_addr = "fe80::101b:6c28:18f4:48a6";
     
     if (inet_pton(AF_INET6, ipv6_addr, &(sock_addr.sin6_addr)) == 1)
     {
